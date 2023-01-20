21da200fc7ff52dca56f930fbc312bb5f126fb42
xCarter93 <xcarter93@gmail.com>
2023-01-20 11:13:30 -0500

    Added initial styling and component structure for Keeper project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Heading from "./Heading";
+import Footer from "./Footer";
+import Note from "./Note";
 
 function App() {
 	return (
 		<div>
 			<Heading />
+			<Note />
+			<Footer />
 		</div>
 	);
 }
--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,29 +1,10 @@
 import React from "react";
 
 function Heading() {
-	const date = new Date();
-	const timeOfDay = date.getHours();
-
-	let customStyle = {
-		color: "",
-	};
-	let greeting;
-
-	if (timeOfDay < 12) {
-		greeting = "Good Morning";
-		customStyle.color = "red";
-	} else if (timeOfDay < 18) {
-		greeting = "Good Afternoon";
-		customStyle.color = "green";
-	} else {
-		greeting = "Good Evening";
-		customStyle.color = "blue";
-	}
-
 	return (
-		<h1 className="heading" style={customStyle}>
-			{greeting}
-		</h1>
+		<header>
+			<h1>Keeper App</h1>
+		</header>
 	);
 }
 
