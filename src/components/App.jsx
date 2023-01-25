c116d769830e2ea6fb43098796b7904581130cbc
xCarter93 <xcarter93@gmail.com>
2023-01-25 07:01:30 -0500

    Added additional components and added functionality for adding new notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,28 @@ import React from "react";
 import Heading from "./Heading";
 import Footer from "./Footer";
 import Note from "./Note";
+import CreateArea from "./CreateArea";
+import notes from "./notes";
+import { useState } from "react";
 
 function App() {
+	const [notes, setNotes] = useState([]);
+
+	function addNote(newNote) {
+		setNotes((prevNotes) => {
+			return [...prevNotes, newNote];
+		});
+	}
+
+	function deleteNote() {}
+
 	return (
 		<div>
 			<Heading />
-			<Note />
+			<CreateArea onAdd={addNote} />
+			{notes.map((noteItem) => {
+				return <Note title={noteItem.title} content={noteItem.content} />;
+			})}
 			<Footer />
 		</div>
 	);
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
-function Note() {
+function Note(props) {
 	return (
 		<div className="note">
-			<h1>Note Title</h1>
-			<p>Note Body</p>
+			<h1>{props.title}</h1>
+			<p>{props.content}</p>
+			<button>DELETE</button>
 		</div>
 	);
 }
